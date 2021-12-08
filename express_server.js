7975ee0e479d23a2d18fe11663928b5fa02aa404
ImAfif <afifadelayoub@gmail.com>
2021-12-08 21:52:04 +0000

    implemented login button, username text section, and returned username upon login

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const bodyParser = require("body-parser");
 const { json } = require("express");
-
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -8,6 +8,8 @@ const PORT = 8080; // default port 8080
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+app.use(cookieParser());
 
 let urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -31,21 +33,29 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  let username = req.cookies["username"];
+
+  console.log(username);
+  const templateVars = { urls: urlDatabase, username: username };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let username = req.cookies["username"];
+  const templateVars = { username: username };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+  let username = req.cookies["username"];
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[req.params.shortURL];
   const templateVars = {
     shortURL,
     longURL,
+    username,
   };
+
   res.render("urls_show", templateVars);
 });
 
@@ -74,6 +84,17 @@ app.post("/urls/:shortURL", (req, res) => {
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/`);
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect(`/urls`);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect(`/urls`);
 });
 
 function generateRandomString() {
