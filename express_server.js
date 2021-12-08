d6a573888eb5357fd32ad9852bf784d99c361511
ImAfif <afifadelayoub@gmail.com>
2021-12-08 23:47:22 +0000

    implemented user and password, also created a new page after login

--- express_server.js
+++ express_server.js
@@ -16,6 +16,19 @@ let urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -95,6 +108,25 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect(`/urls`);
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const user = {
+    id: generateRandomString(),
+    email: req.body.email,
+    password: req.body.password,
+  };
+  const stringifyuser = JSON.stringify(user);
+  users[user.id] = user;
+
+  res.cookie("user_id", user.id);
+  res.redirect(`/register`);
+  console.log(users);
+  console.log(user);
 });
 
 function generateRandomString() {
